c1922369fadae5b09fdb66e31cbb8e519b9671aa
Jack Saunders <jack.saunders.home@live.co.uk>
2021-09-18 19:18:28 +0100

    removing lodash from script

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-var _ = require('lodash');
-
-var array = [1,2,3,4,5,6,7,8];
-console.log('answer: ', _.without(array, 3));
-
 var css = document.querySelector("h3");
 var color1 = document.getElementById("color1");
 var color2 = document.getElementById("color2");
